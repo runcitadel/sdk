85076370dbddb40499c99657a1e2afc94248bfd0
Aaron Dewes <aaron.dewes@web.de>
2021-10-03 14:15:32 +0100

    Add LND info

--- src/middleware/lnd.ts
+++ src/middleware/lnd.ts
@@ -1,11 +1,16 @@
 import { ApiConnection } from "platform/connection.js";
 import { LNDChannel } from "./lnd/channel.js";
+import { LNDInfo } from "./lnd/info.js";
 
 export class MiddlewareLND extends ApiConnection {
   #channel: LNDChannel;
+  #info: LNDInfo;
   constructor(baseUrl: string) {
     super(`${baseUrl}${baseUrl.endsWith("/") ? "" : "/"}v1/lnd`);
     this.#channel = new LNDChannel(
+      `${baseUrl}${baseUrl.endsWith("/") ? "" : "/"}v1/lnd`
+    );
+    this.#info = new LNDInfo(
       `${baseUrl}${baseUrl.endsWith("/") ? "" : "/"}v1/lnd`
     );
   }
@@ -13,6 +18,7 @@ export class MiddlewareLND extends ApiConnection {
   public set jwt(newJwt: string) {
     this._jwt = newJwt;
     this.#channel.jwt = newJwt;
+    this.#info.jwt = newJwt;
   }
 
   public async address(): Promise<string> {
@@ -25,5 +31,9 @@ export class MiddlewareLND extends ApiConnection {
 
   public get channel(): LNDChannel {
     return this.#channel;
+  }
+
+  public get info(): LNDInfo {
+    return this.#info;
   }
 }
