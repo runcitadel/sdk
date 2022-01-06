5b6a5d0066440caeeee7229e68489c83dbb1a350
Aaron Dewes <aaron.dewes@protonmail.com>
2022-01-06 16:50:21 +0100

    Add registerAddress method

--- src/manager/external.ts
+++ src/manager/external.ts
@@ -1,6 +1,10 @@
 import { ApiConnection } from "platform/connection.js";
 import { joinUrl } from "../common/utils.js";
 
+type LnAddressSignupResponse = 'Address added successfully'
+| 'Error: Address limit reached'
+| 'Error: Address already in use'
+| 'Error: Onion URL already used';
 export class ManagerExternal extends ApiConnection {
   constructor(baseUrl: string) {
     super(joinUrl(baseUrl, `v1/external`));
@@ -16,5 +20,14 @@ export class ManagerExternal extends ApiConnection {
     return (
       await this.get<Record<string, number>>(`/price?currency=${currency}`)
     )[currency];
+  }
+
+  /**
+   * Registers a free Lightning address @ln.runcitadel.space and @⚡🏰.ml
+   * 
+   * @param address The address to register (without the @ and the domain)
+   */
+  async registerAddress(address: string): Promise<LnAddressSignupResponse> {
+    return (await this.get<{ msg: LnAddressSignupResponse }>(`/register-address?address=${address}`)).msg;
   }
 }
