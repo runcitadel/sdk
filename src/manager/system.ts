b940fe80927bb1bc0c589a6cf4e7ccfc849dec5a
Aaron Dewes <aaron.dewes@protonmail.com>
2022-06-17 07:38:27 +0200

    Clean getUpdateChannel API (#8)
    Remove unused function argument

--- src/manager/system.ts
+++ src/manager/system.ts
@@ -104,7 +104,7 @@ export class ManagerSystem extends ApiConnection {
       .externalStorage;
   }
 
-  async getUpdateChannel(channel: string): Promise<string> {
+  async getUpdateChannel(): Promise<string> {
     return (await this.get<{channel: string}>('update-channel')).channel;
   }
 
